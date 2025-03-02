3c16fd84251880e4d2e7cc3c83b0d0bd8836fe56
Aayush Gupta <guptaaayush432@gmail.com>
2025-03-02 23:19:26 +0530

    updated L1BossBridge.sol

--- src/L1BossBridge.sol
+++ src/L1BossBridge.sol
@@ -20,15 +20,18 @@ import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { L1Vault } from "./L1Vault.sol";
 
 contract L1BossBridge is Ownable, Pausable, ReentrancyGuard {
-    uint256 public DEPOSIT_LIMIT = 100_000 ether;
+    using SafeERC20 for IERC20;
 
-    IERC20 public immutable token;
+    uint256 public DEPOSIT_LIMIT = 100_000 ether; // e depositing tokens, you can't do too many
+
+    IERC20 public immutable token; // e one bridge per token
     L1Vault public immutable vault;
-    mapping(address account => bool isSigner) public signers;
+    mapping(address account => bool isSigner) public signers; // Users who can "send" a token from L2 -> L1.
 
     error L1BossBridge__DepositLimitReached();
     error L1BossBridge__Unauthorized();
@@ -51,20 +54,47 @@ contract L1BossBridge is Ownable, Pausable, ReentrancyGuard {
         _unpause();
     }
 
+    // q what happens if the owner disbale an account mid-flight?
     function setSigner(address account, bool enabled) external onlyOwner {
         signers[account] = enabled;
     }
 
+    /*
+     * @notice Locks tokens in the vault and emits a Deposit event
+     * the unlock event will trigger the L2 minting process. There are nodes listening
+     * for this event and will mint the corresponding tokens on L2. This is a centralized process.
+     * 
+     * @param from The address of the user who is depositing tokens
+     * @param l2Recipient The address of the user who will receive the tokens on L2
+     * @param amount The amount of tokens to deposit
+     */
+
+    // @audit
+    // Alice: approve token -> bridge
+    // Bob: depositTokensToL2(from: Alice, l2Recipient: Bob, amount: all Alice Approved Token amount)
+    // @audit-high Impact: High Likelihood: High
+    // if a user approves the bridge, any other user can steal their funds
     function depositTokensToL2(address from, address l2Recipient, uint256 amount) external whenNotPaused {
         if (token.balanceOf(address(vault)) + amount > DEPOSIT_LIMIT) {
             revert L1BossBridge__DepositLimitReached();
         }
-        token.transferFrom(from, address(vault), amount);
+        token.safeTransferFrom(from, address(vault), amount);
 
         // Our off-chain service picks up this event and mints the corresponding tokens on L2
         emit Deposit(from, l2Recipient, amount);
     }
 
+    /*
+     * @notice This is the function responsible for withdrawing tokens from L2 to L1.
+     * Our L2 will have a similar mechanism for withdrawing tokens from L1 to L2.
+     * @notice The signature is required to prevent replay attacks. 
+     * 
+     * @param to The address of the user who will receive the tokens on L1
+     * @param amount The amount of tokens to withdraw
+     * @param v The v value of the signature
+     * @param r The r value of the signature
+     * @param s The s value of the signature
+     */
     function withdrawTokensToL1(address to, uint256 amount, uint8 v, bytes32 r, bytes32 s) external {
         sendToL1(
             v,
@@ -78,7 +108,15 @@ contract L1BossBridge is Ownable, Pausable, ReentrancyGuard {
         );
     }
 
-    function sendToL1(uint8 v, bytes32 r, bytes32 s, bytes memory message) public whenNotPaused nonReentrant {
+    /*
+     * @notice This is the function responsible for withdrawing ETH from L2 to L1.
+     *
+     * @param v The v value of the signature
+     * @param r The r value of the signature
+     * @param s The s value of the signature
+     * @param message The message/data to be sent to L1 (can be blank)
+     */
+    function sendToL1(uint8 v, bytes32 r, bytes32 s, bytes memory message) public nonReentrant whenNotPaused {
         address signer = ECDSA.recover(MessageHashUtils.toEthSignedMessageHash(keccak256(message)), v, r, s);
 
         if (!signers[signer]) {
